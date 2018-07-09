cc24d4c62385d213293454c5157ce0decee24b74
liujunnuist <liujunnuist@163.com>
2018-07-09 15:03:15 +0800

    First Commit

---
+++ TextXcode/TextXcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TextXcode
+//
+//  Created by 刘军 on 2018/6/22.
+//  Copyright © 2018年 liujun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World liujunnuist !\n";
+   // return 0;
+}

5c1d2bd1f483ac07fc273ba709d670c00a131c4c
liujunnuist <liujunnuist@163.com>
2018-07-09 15:29:44 +0800

    modified

--- TextXcode/TextXcode/main.cpp
+++ TextXcode/TextXcode/main.cpp
@@ -10,6 +10,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World  Ambr developer !\n";
+    std::cout << "Hello, World  Ambr developer is no 1 !\n";
    // return 0;
 }
